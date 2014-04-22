877e64bb8093afa2c35359c978c97d57bce213af
Soma Stagel <soma@appmaintainers.com>
2014-04-22 12:08:36 +0200

    parent-children relation added to page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :load_pages
+
+  def load_pages
+    @nav_pages = Page.all.to_a.select{|page| page.parent_id.blank?}
+  end
 end
